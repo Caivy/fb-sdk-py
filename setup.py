020331af67089a3d57df01b7714bc1c9b1bd0e8f
Caivy <samnangkhath5@gmail.com>
2021-09-23 11:19:46 +0700

    Update seup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ from setuptools import setup
 
 setup(
     name='fb-sdk-py',
-    version="0.0.1",                # noqa: F821
+    version="0.0.1",             
     description='This client library is designed to support the Facebook Graph API',      
     author='Caivy',
     url='https://github.com/Caivy/fb-sdk-py',
     license='Apache',
-    packages=["facebook-sdk"],
+    packages=["fb_sdk"],
     long_description=open("README.md").read(),
+    keywords=["python", "facebook", "API", "Faccebook Graph API", "requests"],
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
     ],
     install_requires=['requests']
 )
