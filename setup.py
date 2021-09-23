e53f8ecbd5bf63847772ace3dc6f4d2dcba44765
Caivy <samnangkhath5@gmail.com>
2021-09-23 12:08:25 +0700

    Fix Version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='facebook-sdk-py',
-    version="0.0.1",             
+    version="0.0.2",             
     description='This client library is designed to support the Facebook Graph API',      
     author='Caivy',
     url='https://github.com/Caivy/fb-sdk-py',
